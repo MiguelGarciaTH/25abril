07bc807a62d6944da9bd28ff7b1670953f425b0b
Miguel Garcia <mghenriques@sensei.tech>
2023-12-02 15:35:15 +0000

    to squash

---
+++ arquivo-crawler/src/main/java/arquivo/crawler/ArquivoCrawler.java
@@ -0,0 +1,12 @@
+package arquivo.crawler;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArquivoCrawler {
+
+    ArquivoCrawler() {
+        System.out.println("HELLO");
+    }
+
+}
