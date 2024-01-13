7c94462665de56fcd28bbb4221c12521103835ce
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2024-01-13 15:30:46 +0000

    fix(rest): fixes rest server cors

--- arquivo-rest/src/main/java/arquivo/Application.java
+++ arquivo-rest/src/main/java/arquivo/Application.java
@@ -3,6 +3,10 @@ package arquivo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan("arquivo")
@@ -12,4 +16,14 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+}
+
+@Configuration
+@EnableWebMvc
+class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
 }
