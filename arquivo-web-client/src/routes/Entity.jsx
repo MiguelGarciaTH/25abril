28ce1630469fa08d8bfa046c9a31e0f0ab9f4f84
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2024-01-20 13:22:44 +0000

    feat(web): minor improvements to web server

--- arquivo-web-client/src/routes/Entity.jsx
+++ arquivo-web-client/src/routes/Entity.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router";
 
 const Entity = () => {
-    const [entity, setPosts] = useState([]);
+    const [entities, setPosts] = useState([]);
     const { type } = useParams();
 
     useEffect(() => {
@@ -14,14 +14,24 @@ const Entity = () => {
             });
     }, [type]);
 
-    if (entity.length) {
+    if (entities.length) {
         return (
             <div>
                 <h1>{type}</h1>
                 <div>
-                    {entity.map((post) => (
-                        <div className="post" key={post.id}>
-                            <Link className="navbar-link" to={`/articles/${post.id}`}>{post.name}</Link>
+                    {entities.map((entity) => (
+
+                        <div class="box" key={entity.id}>
+                            {console.log("teste  " + entity.imageUrl)}
+                            <div class="box-top">
+                                <img class="box-image" src={entity.imageUrl} alt={`${entity.name}`}></img>
+                            <div class="title-flex">
+                            <h3 class="box-title">{entity.name}</h3>
+                            <p class="user-follow-info">{type}</p>
+                            </div>
+                            <p class="description">Whipped steamed roast cream beans macchiato skinny grinder café. Iced grinder go mocha steamed grounds cultivar panna aroma.</p>
+                            </div>
+                            <Link className="navbar-link" to={`/articles/${entity.id}`}>Artigos</Link>
                         </div>
                     ))}
                 </div>
