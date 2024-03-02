a26e99a7c75b54b5c73e20e1fb679b98f51d2288
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2024-03-02 08:44:43 +0000

    feat(web): improves entities css

--- arquivo-web-client/src/components/Entity.jsx
+++ arquivo-web-client/src/components/Entity.jsx
@@ -4,26 +4,17 @@ import ArticlesCounter from "./ArticlesCounter";
 function Entity({ entityId, entityName, entityImage, entityBio }) {
 
     return (
-        <table class="entity-table" border={0}>
-            <tr>
-                <td colSpan={2}>
-                    <h3>{entityName}</h3>
-                </td>
-            </tr>
-            <tr>
-                <td class="entity-thumb">
-                    <img src={entityImage} alt={`${entityName}`}></img>
-                </td>
-                <td>
-                    <div class="entity-description">{entityBio}</div>
-                </td>
-            </tr>
-            <tr>
-                <td colSpan={2}>
-                    <Link className="entity-button" to={`/articles/${entityId}/${entityName}`}>Artigos(<ArticlesCounter entityId={entityId} />)</Link>
-                </td>
-            </tr>
-        </table>
+        <div class="paper">
+            <div class="paper-content">
+                <h1>{entityName}</h1>
+                <textarea>{entityBio}</textarea>
+                <Link class="paper-button" to={`/articles/${entityId}/${entityName}`}>Artigos(<ArticlesCounter entityId={entityId} />)</Link>
+            </div>
+            <div class="item">
+                <div class="polaroid"><img src={entityImage} alt={`${entityName}`}></img></div>
+            </div>
+        </div>
+        
     );
 };
 
