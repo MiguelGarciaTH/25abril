b10153d65514bae1c8f109fd8722680d4c39e639
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2024-03-24 07:19:43 +0000

    fix

--- arquivo-web-client/vite.config.js
+++ arquivo-web-client/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dns from 'dns';
+//import dns from 'dns';
 
 // localhost part
-dns.setDefaultResultOrder('verbatim');
+//dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     cors: false,
-    host: "13.50.15.148",
+    host: true,
     port: 80,
   },
 })
