0cddad16b0d9b2e363fac8bb0a1f9ed7064b94a6
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2024-04-24 22:17:03 +0100

    fix(web): attempt to fix

--- arquivo-web-client/vite.config.js
+++ arquivo-web-client/vite.config.js
@@ -9,7 +9,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-  //  cors: false,
+    cors: false,
     host: true,
     port: 8000
   },
