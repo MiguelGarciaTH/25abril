2bbeda2c597106f97e8bb3937090b1c2adf3ae42
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2024-12-01 09:17:24 +0000

    feat(text): bootstrap arquivo-text module

--- scripts/package-all.sh
+++ scripts/package-all.sh
@@ -9,6 +9,7 @@ mvn -DskipTests=true -T 4 clean install
 
 (cd arquivo-crawler && mvn -DskipTests=true package jdeb:jdeb)
 (cd arquivo-processor && mvn -DskipTests=true package jdeb:jdeb)
+(cd arquivo-text && mvn -DskipTests=true package jdeb:jdeb)
 (cd arquivo-rest && mvn -DskipTests=true package jdeb:jdeb)
 
 echo
