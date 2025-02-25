fa8dd9ff3914bb1aa35adccb33cad5d06313bcf4
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2025-02-25 19:07:38 +0000

    feat(web): adds article image previews to pop up

--- arquivo-web/src/components/ArticlePreview.jsx
+++ arquivo-web/src/components/ArticlePreview.jsx
@@ -1,18 +1,35 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 import "../Article.css";
 
 
 function ArticlePreview({ item }) {
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
+
+
         <div class="collumn">
-            <div class="head">
+
+            <div
+                className="head"
+                onMouseEnter={() => setIsHovered(true)} // Trigger hover state
+                onMouseLeave={() => setIsHovered(false)} // Reset hover state
+                style={{ position: 'relative' }} // Set relative positioning for the pop-up to work
+            >
                 <span class="headline hl3">
-                <Link class="headline hl3" to={item.articleDetail.url}>{item.articleDetail.title}</Link>
+                    <Link class="headline hl3"
+                        to={item.articleDetail.url}
+                    >{item.articleDetail.title}</Link>
+                    {isHovered && (
+                        <div class="article-preview">
+                            <img src={`${item.articleDetail.imagePath}`} alt={item.articleDetail.title} class="article-preview-popup" />
+                        </div>
+                    )}
                 </span>
                 <p>
                     <span class="headline hl4">{item.articleDetail.site.name}</span>
-                    Relevância: {item.articleDetail.contextualScore*100 + item.articleDetail.summaryScore*10 }
+                    Relevância: {item.articleDetail.contextualScore + item.articleDetail.summaryScore}
                 </p>
             </div>
             <p>
