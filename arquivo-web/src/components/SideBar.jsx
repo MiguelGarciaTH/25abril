f2de2a8c02800160c2aa684e8f9952f1b11a3f05
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2025-03-11 21:24:42 +0000

    feat(web): add icons to side bar

--- arquivo-web/src/components/SideBar.jsx
+++ arquivo-web/src/components/SideBar.jsx
@@ -1,37 +1,45 @@
-import React, { useState } from "react";
-
-import { Link } from "react-router";
-
-import "../SideBar.css"; // Optional for custom styling
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { HiHome } from 'react-icons/hi';
+import { BsPeople } from 'react-icons/bs';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
+import '../SideBar.css';
 
 const Sidebar = () => {
-  const [isVisible, setIsVisible] = useState(false);
+    const [isVisible, setIsVisible] = useState(true);
 
-  const toggleSidebar = () => {
-    setIsVisible(!isVisible);
-    //document.body.style.marginLeft = isVisible ? '0' : '300px';
-  };
+    const toggleSidebar = () => {
+        setIsVisible(!isVisible);
+    };
 
-  return (
-    <div>
-      {/* Hamburger Icon */}
-      <div className={`hamburger-menu ${isVisible ? "open" : ""}`} onClick={toggleSidebar}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-
-      {/* Sidebar */}
-      <div className={`sidebar ${isVisible ? "show" : "hide"}`}>
-        <ul>
-        <li><Link className="sidebar-link" to="/">Home</Link></li>
-        <li><Link className="sidebar-link" to="/about">About</Link></li>
-        <li><Link className="sidebar-link" to="/entities">Entities</Link></li>
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <div className={`hamburger-menu ${isVisible ? 'open' : ''}`} onClick={toggleSidebar}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+            
+            <div className={`sidebar ${isVisible ? 'show' : 'hide'}`}>
+                <div className="sidebar-content">
+                    <div className="icon-links">
+                        <Link to="/home" className="icon-link">
+                            <HiHome className="sidebar-icon" />
+                            <span className="link-text">Home</span>
+                        </Link>
+                        <Link to="/entities" className="icon-link">
+                            <BsPeople className="sidebar-icon" />
+                            <span className="link-text">People</span>
+                        </Link>
+                        <Link to="/about" className="icon-link">
+                            <AiOutlineInfoCircle className="sidebar-icon" />
+                            <span className="link-text">About</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Sidebar;
