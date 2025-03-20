30a14df71f27614e58bf4bb28027be8f16bcc9cb
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2025-03-20 21:23:32 +0000

    fix(web): adds improvements to slider

--- arquivo-web/src/routes/Data.jsx
+++ arquivo-web/src/routes/Data.jsx
@@ -21,26 +21,28 @@ const Data = () => {
       <Header isHome={false} />
       <span style={{ display: "block", height: "100px" }}></span>
 
-      {/* Slider input */}
-      <div className="sliderContainer">
-        <label htmlFor="sizeSlider">Top {size} de artigos</label>
-        <input
-          id="sizeSlider"
-          type="range"
-          min="1"
-          max="100"
-          value={size}
-          onChange={handleSliderChange}
-        />
-      </div>
-      <span style={{ display: "block", height: "20px" }}></span>
+      {/* Flex container for slider and toggle */}
+      <div className="controlsContainer" style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "20px" }}>
 
-      {/* Sliding Toggle Button */}
-      <div className="toggleContainer">
-        <div className="toggleWrapper" onClick={handleToggleClick}>
-          <div className={`toggleSlider ${toggle === "Relevância" ? "left" : "right"}`}></div>
-          <span className="toggleOption">Relevância</span>
-          <span className="toggleOption">Entidades</span>
+        {/* Sliding Toggle Button */}
+        <div className="toggleContainer">
+          <div className="toggleWrapper" onClick={handleToggleClick}>
+            <div className={`toggleSlider ${toggle === "Relevância" ? "left" : "right"}`}></div>
+            <span className="toggleOption">Relevância</span>
+            <span className="toggleOption">Entidades</span>
+          </div>
+        </div>
+        {/* Slider input */}
+        <div className="sliderContainer">
+          <label htmlFor="sizeSlider">Top {size} de artigos</label>
+          <input
+            id="sizeSlider"
+            type="range"
+            min="1"
+            max="100"
+            value={size}
+            onChange={handleSliderChange}
+          />
         </div>
       </div>
       <span style={{ display: "block", height: "50px" }}></span>
