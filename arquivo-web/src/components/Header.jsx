7c60d52d978c6a5be8d07c4be83b241a41c88d5a
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2025-03-22 22:59:59 +0000

    fix(web): fixes header and search form for mobile

--- arquivo-web/src/components/Header.jsx
+++ arquivo-web/src/components/Header.jsx
@@ -1,16 +1,44 @@
 // Header.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router';
 
 import "../styles/Header.css"; // Include the CSS for styling
 
 function Header({ isHome }) {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768); // Adjust breakpoint as needed
+        };
+
+        handleResize(); // Check on initial render
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     if (isHome) {
-        return (
-            <header className="header-page-home">
-                <h1><Link className="header-page-link" to="/">[ARQUIVO 25 DE ABRIL]</Link></h1>
-            </header>
-        );
+        if (isMobile) {
+            const fontSize = 24; // Adjust font size as needed
+            return (
+                <header className="header-page-home" style={{ marginTop: "50%" }}>
+                    <h1>
+                        <span style={{ fontSize: "120px" }}>[</span>
+                        <span style={{ display: "inline-block", textAlign: "center", fontSize:"46px" }}>
+                            Arquivo<br />25 de Abril
+                        </span>
+                        <span style={{ fontSize: "120px" }}>]</span>
+                    </h1>
+                </header>
+            );
+        } else {
+            return (
+                <header className="header-page-home">
+                    <h1><Link className="header-page-link" to="/">[ARQUIVO 25 DE ABRIL]</Link></h1>
+                </header>
+            );
+        }
     } else {
         return (
             <header className="header-page">
