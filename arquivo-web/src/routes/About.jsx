05a0758619bf826f7250a9c6252f671ec05d45a7
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2025-03-28 18:40:58 +0000

    fix(web): improves text and styles from about page

--- arquivo-web/src/routes/About.jsx
+++ arquivo-web/src/routes/About.jsx
@@ -9,16 +9,16 @@ const About = () => {
             <Header isHome={false} />
             <div class="text">
                 <h2>Motivação</h2>
-                <strong>O 25 de Abril de 1974 encerrou um periodo 48 anos em que Portugal viveu em ditadura (7 anos de Ditudura Militar seguidos de 41 anos de Estado Novo), dando lugar ao regime democrático que prevalece até aos nossos dias. </strong>
+                <strong>O 25 de Abril de 1974 encerrou um periodo 48 anos em que Portugal viveu em ditadura (7 anos de Ditudura Militar seguidos de 41 anos de Estado Novo), 
+                    dando lugar ao regime democrático que prevalece até aos nossos dias. </strong>
                 É por isso, o evento mais importante do século XX na História portuguesa.
 
 
                 <br /><br />
-                <strong>O principal objectivo deste projecto é promover a memória do que foi a divulgação do maior acontecimento do secúlo XX em Portugal. </strong>
-                Além disso, este projecto também pretende destacar a relevância da imprensa online como um arquivo histórico através de um olhar jornalistico.
-                A imprensa online é por excelência um meio de comunicação acessivel, em particular aos mais novos.
-                Por isso, é de relevar a importância da imprensa como a fonte de informação verdadeira e isenta.
-                E claro, este projecto também serve de homenagem aos que lutaram por um Portugal livre e sem o qual iniciativas como o Prémio Arquivo.pt não existiriam.
+                <strong>O principal objectivo deste projecto é promover a memória do maior acontecimento do secúlo XX em Portugal através dos artigos da imprensa online. </strong>
+                Portanto, este projecto coloca em destaque a importância imprensa online como um arquivo histórico através de um olhar jornalistico.
+                A imprensa online é por excelência um meio de comunicação acessivel e isento.
+                Não menos importante, este projecto também tem o propósito homenagear os que lutaram por um Portugal livre e sem o qual iniciativas como o Prémio Arquivo.pt e este projecto não existiriam.
 
                 <br /><br />
                 <strong>O Arquivo.pt preserva toda Web Portuguesa desde 1991. </strong>
@@ -50,14 +50,6 @@ const About = () => {
                 e referências aos que <Link to="https://www.museudoaljube.pt/centro-de-documentacao/biografias/">lutaram mas não sobreviveram para contar a história</Link>.
 
 
-                <br /><br />
-                Uma nota final sobre as personalidades selecionadas.
-                Existem várias personalidades que nem sempre estiveram do lado "certo", ou sempre do lado errado.
-                Ou independentemente do lado onde estiveram,
-                tiveram contributos enormes para Portugal, por exemplo, Duarte Pacheco.
-                Outras personalidades que sairam como heróis da revolução e que mais tarde estiveram ligadas a movimentos terroristas no pós-revolução, por exemplo Spinola ou Otelo.
-                No entanto, por uma questão de categorização e organização tentei colocar estas personalidades no que me pareceu ser "a categoria" mais correcta tendo em conta o tema principal: o 25 de Abril.
-
                 <h2>Agradecimentos</h2>
                 Quero agradecer ao historiador Jorge Martins e ao meu mestre José Oliveira, duas pessoas que viveram o 25 de Abril durante a juventude e dispensaram o seu tempo para
                 sugerir nomes e melhorias.
@@ -65,29 +57,34 @@ const About = () => {
 
                 <h2>Referências</h2>
                 <h3>Livros</h3>
-                <li>25 de Abril: Documento (2ª Edição, revista e aumentada). Afonso Praça, Albertino Antunes, António Amori, Cesário Borga, Fernando Cascais</li>
-                <li>Censura - O Lápis Azul do Silêncio. Ana Aranha</li>
-                <li>Portugal no Século XX - Os Anos 70. César Santos Silva</li>
-                <li>Os Últimos do Estado Novo. José Pedro Castanheira</li>
-                <li>1973: Uma Cronologia do Ano Zero. Os Factos, as Figuras e os Figurantes do Último Ano do Estado Novo. Tiago Beato</li>
-                <li>Ensaios de Abril. Fernando Rosas</li>
+                <ul>
+                    <li>25 de Abril: Documento (2ª Edição, revista e aumentada). Afonso Praça, Albertino Antunes, António Amori, Cesário Borga, Fernando Cascais</li>
+                    <li>Censura - O Lápis Azul do Silêncio. Ana Aranha</li>
+                    <li>Portugal no Século XX - Os Anos 70. César Santos Silva</li>
+                    <li>Os Últimos do Estado Novo. José Pedro Castanheira</li>
+                    <li>1973: Uma Cronologia do Ano Zero. Os Factos, as Figuras e os Figurantes do Último Ano do Estado Novo. Tiago Beato</li>
+                    <li>Ensaios de Abril. Fernando Rosas</li>
+                </ul>
 
                 <h3>Podcasts</h3>
-                <li>Histórias da Classe Trabalhadora</li>
-                <li>De Cravo ao Peito</li>
-                <li>Antes da Revolução</li>
-                <li>Inquietação, inquietação!</li>
-                <li>Avenida da Liberdade</li>
-                <li>Retratos de Abril</li>
-                <li>Clandestinos</li>
+                <ul>
+                    <li>Histórias da Classe Trabalhadora</li>
+                    <li>De Cravo ao Peito</li>
+                    <li>Antes da Revolução</li>
+                    <li>Inquietação, inquietação!</li>
+                    <li>Avenida da Liberdade</li>
+                    <li>Retratos de Abril</li>
+                    <li>Clandestinos</li>
+                </ul>
 
                 <h3>Documentários</h3>
-                <li>A Pide Leninha (RTP Play)</li>
-                <li>Prisioneiros de Guerra (RTP Play)</li>
-                <li>Conceição Matos: Coragem hoje, abraços amanhã (RTP Play)</li>
-                <li>Salgueiro Maia - Rumo à Eternidade (RTP Play)</li>
-                <li>Os Últimos Dias da PIDE (RTP Play)</li>
-
+                <ul>
+                    <li>A Pide Leninha (RTP Play)</li>
+                    <li>Prisioneiros de Guerra (RTP Play)</li>
+                    <li>Conceição Matos: Coragem hoje, abraços amanhã (RTP Play)</li>
+                    <li>Salgueiro Maia - Rumo à Eternidade (RTP Play)</li>
+                    <li>Os Últimos Dias da PIDE (RTP Play)</li>
+                </ul>
             </div>
         </div>
     )
