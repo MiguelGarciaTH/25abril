eb18d3a5a17495bd9e5a2fb529777a26546df59c
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2025-04-14 19:03:50 +0100

    fix(sh): improves sync script

--- scripts/deploy-web.sh
+++ scripts/deploy-web.sh
@@ -3,4 +3,4 @@
 set -e
 
 #scp -r arquivo-web arquivo-25-abril-ec2:
-rsync -avz --delete arquivo-web/dist/ arquivo-25-abril-ec2:/var/www/html/arquivo-web/
+rsync -avzL --delete --progress arquivo-web/dist/ arquivo-25-abril-ec2:/var/www/html/arquivo-web/
