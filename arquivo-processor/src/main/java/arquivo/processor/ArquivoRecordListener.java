7d8f0a8fb9ec36b5604c76b40cbb1c81ed2e93de
MiguelGarciaTH <miguel.garcia.th@gmail.com>
2025-04-26 16:53:27 +0100

    fix(processor): changes keyword list and increases the contextual score threshold

--- arquivo-processor/src/main/java/arquivo/processor/ArquivoRecordListener.java
+++ arquivo-processor/src/main/java/arquivo/processor/ArquivoRecordListener.java
@@ -135,7 +135,7 @@ public class ArquivoRecordListener {
 
             final SearchEntity searchEntity = getSearchEntity(event.searchEntityId());
             final TextScoreService.Score score = scoreService.contextualScore(event.title(), event.textUrl(), text, false);
-            if (score.total() > 2) {
+            if (score.total() > 5) {
                 final JsonNode scoreJson = objectMapper.convertValue(score.keywordCounter(), JsonNode.class);
                 article = articleRepository.save(new Article(event.title(), event.url(), trimUrl(event.url()), LocalDateTime.now(ZoneOffset.UTC), site, text, score.total(), scoreJson));
                 LOG.debug("New article articleId={} title={} url={} with score={}", article.getId(), event.title(), event.url(), score);
